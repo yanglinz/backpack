1afcb68c8a9bf086e62331ca973d0b01617ab092
Yanglin Zhao <yz1876@gmail.com>
2019-12-23 13:46:10 -0500

    Implement rootPath

--- internal/context.go
+++ internal/context.go
@@ -1,6 +1,9 @@
 package internal
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 )
 
@@ -10,10 +13,37 @@ type Context struct {
 	Name string
 }
 
+func parseRootPath(cmd *cobra.Command) string {
+	// Get the root project path based on flag or cwd
+	root := ""
+	if cmd.Flag("root") != nil {
+		root = cmd.Flag("root").Value.String()
+	} else {
+		cwd, err := os.Getwd()
+		root = cwd
+		if err != nil {
+			return ""
+		}
+	}
+
+	// Convert to absolute path
+	root, err := filepath.Abs(root)
+	if err != nil {
+		return ""
+	}
+
+	return root
+}
+
 // ParseContext returns Context for a given project
 func ParseContext(cmd *cobra.Command) Context {
+	rootPath := parseRootPath(cmd)
+	if rootPath == "" {
+		panic("Failed to parse project root path")
+	}
+
 	context := Context{
-		Root: "",
+		Root: rootPath,
 		Name: "",
 	}
 	return context
