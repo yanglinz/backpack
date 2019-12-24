8acf9017bbf530e83b9cbce452e182964361cf90
Yanglin Zhao <yanglinz@users.noreply.github.com>
2019-12-24 17:54:21 -0500

    Implement image publishing to gcp (#17)
    * Publish APP_NAME
    
    * Add release branch logic
    
    * Add feature/publish
    
    * Add gcp project id
    
    * Try pushing latest image to gcp
    
    * Master branch

--- configs/github/build-release.sh
+++ configs/github/build-release.sh
@@ -2,21 +2,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+RELEASE_BRANCH="master"
+SOURCE_IMAGE="source-image"
+DOCKER_REPO="gcr.io/${GCP_PROJECT_ID}/${APP_NAME}"
+
 function build_release() {
   docker build \
     -f .backpack/configs/docker/python-prod.Dockerfile \
-    --tag application-release \
+    --tag "$SOURCE_IMAGE" \
     .
 }
 
 function publish_release() {
   gcloud auth configure-docker
-  # docker tag [SOURCE_IMAGE] [HOSTNAME]/[PROJECT-ID]/[IMAGE]
-  # docker tag [SOURCE_IMAGE] [HOSTNAME]/[PROJECT-ID]/[IMAGE]:[TAG]
-  # SOURCE_IMAGE = application-release
-  # HOSTNAME = gcr.io
-  # PROJECT-ID = default-123
-  # IMAGE = application-name
+  docker tag "$SOURCE_IMAGE" "$DOCKER_REPO"
+  docker push "$DOCKER_REPO"
 }
 
 build_release
+
+if [ "$GITHUB_REF" == "refs/heads/${RELEASE_BRANCH}" ]; then
+  publish_release
+else
+  echo "Not on ${RELEASE_BRANCH} branch. Nothing to build."
+fi
