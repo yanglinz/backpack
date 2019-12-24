fb3c60e26fe7d6720e1ea83624c21326041b6898
Yanglin Zhao <yz1876@gmail.com>
2019-12-24 15:35:58 -0500

    Add pseudo code on production release

--- configs/github/build-release.sh
+++ configs/github/build-release.sh
@@ -5,8 +5,18 @@ IFS=$'\n\t'
 function build_release() {
   docker build \
     -f .backpack/configs/docker/python-prod.Dockerfile \
-    --tag hello-world \
+    --tag application-release \
     .
+}
+
+function publish_release() {
+  gcloud auth configure-docker
+  # docker tag [SOURCE_IMAGE] [HOSTNAME]/[PROJECT-ID]/[IMAGE]
+  # docker tag [SOURCE_IMAGE] [HOSTNAME]/[PROJECT-ID]/[IMAGE]:[TAG]
+  # SOURCE_IMAGE = application-release
+  # HOSTNAME = gcr.io
+  # PROJECT-ID = default-123
+  # IMAGE = application-name
 }
 
 build_release
