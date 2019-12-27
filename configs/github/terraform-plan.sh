fff279f1131b8541733093a2c0ce3531448a0feb
Yanglin Zhao <yanglinz@users.noreply.github.com>
2019-12-26 21:27:32 -0500

    Only run github apply (#20)
    * Only run github apply
    
    * Add empty whitespace
    
    * Output version
    
    * Try to install tfenv
    
    * Install tfenv version
    
    * Hash files
    
    * Add terraform version tag
    
    * Create main.yml
    
    * Fix naming
    
    * Fix
    
    * Already in dir
    
    * Master -> apply
    
    * Revert "Master -> apply"
    
    bba46a72d5e1046fbe7b91a8c5d4c8c6224ad32c

--- configs/github/terraform-plan.sh
+++ configs/github/terraform-plan.sh
@@ -4,4 +4,8 @@ IFS=$'\n\t'
 
 echo "$GCP_SERVICE_ACCOUNT_KEY" | base64 --decode > /tmp/service-account.json
 export GCLOUD_KEYFILE_JSON="/tmp/service-account.json"
-cd terraform && terraform plan
+
+cd terraform
+terraform init -backend-config="token=${TERRAFORM_CLOUD_TOKEN}"
+terraform plan
+cd -
