5f6f2f06ba28f2b0a190062b36ab9a23174903dc
Yanglin Zhao <yanglinz@users.noreply.github.com>
2019-12-26 21:49:45 -0500

    Tag docker image artifact based on file hash (#21)
    * Push based on RELEASE_TAG
    
    * Test on branch
    
    * Use the right bash name
    
    * Revert "Test on branch"
    
    67b94e2214fcd58387c2ff63ab583dc60cb9aea6

--- configs/github/build-release.sh
+++ configs/github/build-release.sh
@@ -5,6 +5,7 @@ IFS=$'\n\t'
 RELEASE_BRANCH="master"
 SOURCE_IMAGE="source-image"
 DOCKER_REGISTRY="gcr.io/${GCP_PROJECT_ID}/${APP_NAME}"
+RELEASE_TAG="$(. "$(dirname "$0")/hash-files.sh")"
 
 function build_release() {
   docker build \
@@ -16,7 +17,9 @@ function build_release() {
 function publish_release() {
   gcloud auth configure-docker
   docker tag "$SOURCE_IMAGE" "$DOCKER_REGISTRY"
+  docker tag "$SOURCE_IMAGE" "$DOCKER_REGISTRY:${RELEASE_TAG}"
   docker push "$DOCKER_REGISTRY"
+  docker push "$DOCKER_REGISTRY:${RELEASE_TAG}"
 }
 
 build_release
