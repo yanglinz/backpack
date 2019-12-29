036704b320a23aaae58f68877ad59f34d65b0490
Yanglin Zhao <yz1876@gmail.com>
2019-12-29 00:40:32 -0500

    Try setting debug

--- runtime/berglas-loader.sh
+++ runtime/berglas-loader.sh
@@ -6,7 +6,7 @@ echo "Plain env"
 env
 
 echo "Berglas env"
-berglas exec -- env
+berglas --log-level=debug exec -- env
 
 BERGLAS_ENV_PY="$(dirname "$0")/berglas_env.py"
 berglas access "${BERGLAS_BUCKET}/BERGLAS_APP_JSON" > /tmp/berglas-app.json
