971789ca25a15356b03d9e56b37541fac4a103ed
Yanglin Zhao <yz1876@gmail.com>
2020-01-05 23:19:55 -0500

    Load secrets in berglas directly

--- runtime/berglas-loader.sh
+++ runtime/berglas-loader.sh
@@ -4,6 +4,5 @@ IFS=$'\n\t'
 
 BERGLAS_ENV_PY="$(dirname "$0")/berglas_env.py"
 
-berglas exec -- echo "$BERGLAS_APP_JSON" > /tmp/berglas-app.json
-cat /tmp/berglas-app.json
+berglas access "$BERGLAS_SECRET_PATH" > /tmp/berglas-app.json
 eval $(python3 "$BERGLAS_ENV_PY" /tmp/berglas-app.json)
