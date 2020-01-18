67424f408a4ad2698f7dc5ce2ee2cdca921f9d9d
Yanglin Zhao <yz1876@gmail.com>
2020-01-17 22:09:28 -0500

    Add the heroku yml

--- runtime/berglas-loader.sh
+++ runtime/berglas-loader.sh
@@ -2,5 +2,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-berglas access "$BERGLAS_SECRET_PATH" > /tmp/berglas-app.json
-eval $(python3 "$(dirname "$0")/load_env.py" /tmp/berglas-app.json)
+function load_vars() {
+  berglas access "$BERGLAS_SECRET_PATH" > /tmp/berglas-app.json
+  eval $(python3 "$(dirname "$0")/load_env.py" /tmp/berglas-app.json)
+}
+
+if [[ -n "${BERGLAS_SECRET_PATH+set}" ]]; then
+  load_vars
+fi
