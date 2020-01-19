d68f447c1819596c80be678c299c7a9907357bbd
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-01-18 19:56:06 -0500

    Implement deployment to heroku (#38)
    * Add a final deploy step
    
    * Progress
    
    * Publish gcp registry
    
    * Build name
    
    * Heroku
    
    * Install heroku
    
    * Change to API token
    
    * Final tweak to build-release
    
    * Message

--- github/build-release.sh
+++ github/build-release.sh
@@ -6,6 +6,12 @@ RELEASE_BRANCH="master"
 SOURCE_IMAGE="source-image"
 DOCKER_REGISTRY="gcr.io/${GCP_PROJECT_ID}/${APP_NAME}"
 RELEASE_TAG="$(. "$(dirname "$0")/hash-files.sh")"
+HEROKU_APP_NAME="${APP_NAME}-backpack"
+
+function debug_info() {
+  sudo apt-get update && sudo apt-get install tree
+  tree -d -I node_modules
+}
 
 function build_release() {
   docker build \
@@ -14,20 +20,38 @@ function build_release() {
     .
 }
 
-function publish_release() {
+function publish_gcp_registry() {
   gcloud auth configure-docker
   docker tag "$SOURCE_IMAGE" "$DOCKER_REGISTRY"
-  docker tag "$SOURCE_IMAGE" "$DOCKER_REGISTRY:${RELEASE_TAG}"
+  docker tag "$SOURCE_IMAGE" "${DOCKER_REGISTRY}:${RELEASE_TAG}"
   docker push "$DOCKER_REGISTRY"
-  docker push "$DOCKER_REGISTRY:${RELEASE_TAG}"
+  docker push "${DOCKER_REGISTRY}:${RELEASE_TAG}"
 }
 
+function publish_deploy_heroku() {
+  bash "$(dirname "$0")/install-heroku.sh"
+
+  # Push to the container registry
+  heroku container:login
+  docker tag "$SOURCE_IMAGE" "registry.heroku.com/${HEROKU_APP_NAME}/web"
+  docker tag "$SOURCE_IMAGE" "registry.heroku.com/${HEROKU_APP_NAME}/worker"
+  docker push "registry.heroku.com/${HEROKU_APP_NAME}/web"
+  docker push "registry.heroku.com/${HEROKU_APP_NAME}/worker"
+ 
+ # Release the build
+  heroku container:release web
+  heroku container:release worker
+}
+
+debug_info
 build_release
 
 if [ "$GITHUB_REF" != "refs/heads/${RELEASE_BRANCH}" ]; then
   echo "Not on ${RELEASE_BRANCH} branch. Nothing to publish."
+elif [ "$RUNTIME_PLATFORM" == "CLOUD_RUN"]; then
+  publish_gcp_registry
 elif [ "$RUNTIME_PLATFORM" == "HEROKU"]; then
-  echo "Not on GCP. Nothing to publish."
+  publish_deploy_heroku
 else
-  publish_release
+  echo "Not on GCP or Heroku. Nothing to publish."
 fi
