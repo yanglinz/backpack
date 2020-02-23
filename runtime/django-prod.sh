816fd3fa2f46a011ecb9ffbf0a054ebf4de7a431
Yanglin Zhao <yz1876@gmail.com>
2020-02-23 14:29:28 -0500

    Copy build directory on startup

--- runtime/django-prod.sh
+++ runtime/django-prod.sh
@@ -4,6 +4,8 @@ IFS=$'\n\t'
 
 # Manage static files
 python manage.py collectstatic --noinput
+[ -d "./build" ] && cp -r ./build/. ./www
+rm -f ./www/index.html  # Disallow index.html in static files
 
 # Run migration
 python manage.py migrate
