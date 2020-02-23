071732d3b0df9a8c71113fa01eec7e0ab2042872
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-02-22 22:10:07 -0500

    Run collectstatic on application startup (#46)

---
+++ docker/scripts/run-prod.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Manage static files
+pipenv run python manage.py collectstatic
+
+# Start production server
+uwsgi --ini /app/.backpack/docker/uwsgi/uwsgi.ini
