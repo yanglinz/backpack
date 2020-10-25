e01656df4953ab2d084dab30197d27ca659c05ec
Yanglin Zhao <yz1876@gmail.com>
2020-10-25 11:27:56 -0400

    Add missing dokku setup

--- digitalocean/dokku-setup.sh
+++ digitalocean/dokku-setup.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 dokku ssh-keys:add backpack ~/.ssh/authorized_keys || true
+dokku ssh-keys:add "$APP_NAME" ~/.ssh/authorized_keys || true
+dokku git:initialize "$APP_NAME"
