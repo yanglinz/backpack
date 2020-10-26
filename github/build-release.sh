3c3cba1c4c26f58274ba681c0a330812c4ab51d6
Yanglin Zhao <yz1876@gmail.com>
2020-10-25 22:08:29 -0400

    Use main as the release branch when building artifacts

--- github/build-release.sh
+++ github/build-release.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-RELEASE_BRANCH="master"
+RELEASE_BRANCH="main"
 SOURCE_IMAGE="source-image"
 DOCKER_REGISTRY="gcr.io/${GCP_PROJECT_ID}/${APP_NAME}"
 RELEASE_TAG="$(. "$(dirname "$0")/hash-files.sh")"
@@ -37,7 +37,7 @@ function publish_deploy_heroku() {
   docker tag "$SOURCE_IMAGE" "registry.heroku.com/${HEROKU_APP_NAME}/worker"
   docker push "registry.heroku.com/${HEROKU_APP_NAME}/web"
   docker push "registry.heroku.com/${HEROKU_APP_NAME}/worker"
- 
+
  # Release the build
   heroku container:release web -a "$HEROKU_APP_NAME"
   heroku container:release worker -a "$HEROKU_APP_NAME"
@@ -50,7 +50,7 @@ function generate_vm_artifact() {
   ./backpack vars get --env=production > var/env/production.json
 
   # Create tarball
-  tar -zcvf app-artifact.tar.gz $(cwd)
+  tar -zcvf app-artifact.tar.gz $(pwd)
 }
 
 debug_info
@@ -58,12 +58,15 @@ debug_info
 if [[ "$GITHUB_REF" != "refs/heads/${RELEASE_BRANCH}" ]]; then
   echo "Not on ${RELEASE_BRANCH} branch. Nothing to publish."
 elif [[ "$RUNTIME_PLATFORM" == "CLOUD_RUN" ]]; then
+  echo "Building release artifact for CLOUD_RUN"
   build_release
   publish_gcp_registry
 elif [[ "$RUNTIME_PLATFORM" == "HEROKU" ]]; then
+  echo "Building release artifact for HEROKU"
   build_release
   publish_deploy_heroku
 elif [[ "$RUNTIME_PLATFORM" == "VM" ]]; then
+  echo "Building release artifact for VM"
   generate_vm_artifact
 else
   echo "Not on GCP or Heroku. Nothing to publish."
