24b628855161c333e22b77df6c5f36f5474d037b
Yanglin Zhao <yz1876@gmail.com>
2020-10-25 23:52:56 -0400

    Fix dokku-deploy.sh

--- digitalocean/dokku-deploy.sh
+++ digitalocean/dokku-deploy.sh
@@ -2,16 +2,28 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-ENV_SOURCE="/opt/backpack-app/var/env/production.json"
+function set_buildpack() {
+  dokku config:set "$APP_NAME" BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python.git#v184
+}
 
-echo "Setting ${APP_NAME} application configs..."
+function set_vars() {
+  local ENV_SOURCE="/opt/backpack-app/var/env/production.json"
 
-for name in $(jq --raw-output 'keys | .[]' "$ENV_SOURCE"); do
-  value=$(jq --raw-output ".${name}" "$ENV_SOURCE")
-  dokku config:set --no-restart "$APP_NAME" "$name"="$value"
-done
+  echo "Setting ${APP_NAME} application configs..."
 
-ls -la /tmp/
+  for name in $(jq --raw-output 'keys | .[]' "$ENV_SOURCE"); do
+    value=$(jq --raw-output ".${name}" "$ENV_SOURCE")
+    dokku config:set --no-restart "$APP_NAME" "$name"="$value"
+  done
+}
+
+function deploy() {
+  cat /tmp/app-artifact.tar.gz | dokku tar:in "$APP_NAME"
+}
+
+set_buildpack
+set_vars
+deploy
 
 dokku config:set "$APP_NAME" TIMESTAMP=$(date +%s)
 dokku domains:enable backpack-example
