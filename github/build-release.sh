547508ae10d267d433dc9ac7b021b45064983fa3
Yanglin Zhao <yz1876@gmail.com>
2020-10-25 22:35:57 -0400

    Try passing different flags

--- github/build-release.sh
+++ github/build-release.sh
@@ -50,7 +50,7 @@ function generate_vm_artifact() {
   ./backpack vars get --env=production > var/env/production.json
 
   # Create tarball
-  tar -zcvf app-artifact.tar.gz $(pwd)
+  tar -czf app-artifact.tar.gz .
 }
 
 debug_info
